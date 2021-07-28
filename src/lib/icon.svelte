e1be7c49dc330a5c26f51bdd875a251df3b0fc8c
Adam Grady <adam.grady@live.com>
2021-07-28 15:47:42 -0500

    Icon Component Fixes #8

---
+++ src/lib/icon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let icon: string;
+	export let size: 'sm' | 'md' | 'lg' | 'fill' = 'fill';
+
+	let width: number;
+	$: switch (size) {
+		case 'sm':
+			width = 12;
+			break;
+		case 'md':
+			width = 24;
+			break;
+		case 'lg':
+			width = 36;
+			break;
+		case 'fill':
+			width = undefined;
+			break;
+	}
+</script>
+
+<svg class="fill-current" viewBox="0 0 24 24" {width} height={width}>
+	<path d={icon} />
+</svg>
